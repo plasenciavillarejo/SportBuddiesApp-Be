0dadfff0ce63e2cbb55d34259a12f2d2a0b1db2b
Jose Plasencia <jose.plasencia@externos.correo.gob.es>
2024-09-11 13:28:10 +0200

    [Plasencia]-[Configuración Proyectos]

--- SportBuddiesApp-Be-Entity/src/main/java/es/sport/buddies/entity/app/SportBuddiesAppBeEntityApplication.java
+++ SportBuddiesApp-Be-Entity/src/main/java/es/sport/buddies/entity/app/SportBuddiesAppBeEntityApplication.java
@@ -1,8 +1,11 @@
 package es.sport.buddies.entity.app;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication
+// Esto es necesario cuando se pretende arrancar el proceso fuera de eclipse, de lo contrario indicara que no encuenta la/s @Entity
+@EntityScan({"es.sport.buddies.entity.app.models.entity"})
+@SpringBootApplication()
 public class SportBuddiesAppBeEntityApplication {
 
 }
