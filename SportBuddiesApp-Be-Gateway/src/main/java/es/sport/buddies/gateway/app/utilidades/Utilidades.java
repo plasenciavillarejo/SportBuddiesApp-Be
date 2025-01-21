af75da83865e34e385a215956dd55a0f3768941b
plasenciavillarejo <plasenciavillarejo@gmail.com>
2025-01-21 17:36:16 +0100

    Corrección seguridad

--- SportBuddiesApp-Be-Gateway/src/main/java/es/sport/buddies/gateway/app/utilidades/Utilidades.java
+++ SportBuddiesApp-Be-Gateway/src/main/java/es/sport/buddies/gateway/app/utilidades/Utilidades.java
@@ -15,9 +15,15 @@ public class Utilidades {
   /**
    * Función encargada de mapear todas las rutas públicas dentro de la aplicacion
    */
-  public List<String> publicRoutes = Arrays.asList("/authorized", "/logout", "/oauth2/**",
-      "/api/main/reservaActividad/listarReserva", "/api/main/reservaActividad/comboInicio",
-      "/api/main/reservaActividad/listadoMunicipios", "/api/main/reservaActividad/listadoReserva",
-      "/api/main/borrarCookie", "/api/main/paypal/estado/pago", "/api/main/usuario/crear");
+  public List<String> publicRoutes = Arrays.asList("/authorized", 
+      "/logout", "/oauth2/**",
+      "/api/main/reservaActividad/listarReserva", 
+      "/api/main/reservaActividad/comboInicio",
+      "/api/main/reservaActividad/listadoMunicipios", 
+      "/api/main/reservaActividad/listadoReserva",
+      "/api/main/borrarCookie", 
+      "/api/main/paypal/estado/pago", 
+      "/api/main/usuario/crear",
+      "/api/oauth2/**");
   
 }
